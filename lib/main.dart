f1fa0bd4aa678e17f922e659d573e8354ec7d729
Sravan <sravanpabolu@gmail.com>
2020-08-15 21:30:28 +0530

    Styling the title and subtitle.

--- lib/main.dart
+++ lib/main.dart
@@ -68,20 +68,35 @@ class MyHomePage extends StatelessWidget {
                           ),
                           decoration: BoxDecoration(
                             border: Border.all(
-                              color: Colors.black,
+                              color: Colors.red,
                               width: 2,
                             ),
                           ),
                           padding: EdgeInsets.all(10),
                           child: Text(
                             tx.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.red,
+                            ),
                           ),
                         ),
                         Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Text(tx.title),
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                             Text(
                               tx.date.toString(),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
                             ),
                           ],
                         )
