99f41d4077d71c8bab570977d5684d96ace8e1c8
Sravan <sravanpabolu@gmail.com>
2020-08-15 20:33:39 +0530

    Creating model class. Adding dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +17,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: "Groceries",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,26 +39,26 @@ class MyHomePage extends StatelessWidget {
           title: Text("Flutter App"),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Container(
-                child: Text(
-                  "Chart!",
-                  // textAlign: TextAlign.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Container(
+                    child: Text(
+                      "Chart!",
+                      // textAlign: TextAlign.center,
+                    ),
+                  ),
+                  elevation: 5,
                 ),
               ),
-              elevation: 5,
-            ),
-          ),
-          Card(
-            color: Colors.orange,
-            child: Text("List of Transactions"),
-          )
-        ]));
+              Card(
+                color: Colors.orange,
+                child: Text("List of Transactions"),
+              )
+            ]));
   }
 }
