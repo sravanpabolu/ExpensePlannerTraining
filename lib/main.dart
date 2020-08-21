2846059073e2e2739eaac34cbeaf6a6f3a9d9c8c
Sravan <sravanpabolu@gmail.com>
2020-08-21 16:19:21 +0530

    Adjusting the chart and transactions list to screen size

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,12 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
           fontFamily: 'Quicksand',
           textTheme: ThemeData.light().textTheme.copyWith(
-                headline6: TextStyle(
-                  fontFamily: "OpenSans",
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-                button: TextStyle(
-                  color: Colors.white
-                )
+              headline6: TextStyle(
+                fontFamily: "OpenSans",
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
               ),
+              button: TextStyle(color: Colors.white)),
           appBarTheme: AppBarTheme(
             textTheme: ThemeData.light().textTheme.copyWith(
                   headline6: TextStyle(
@@ -51,18 +48,66 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransations = [
-    // Transaction(
-    //   id: 't1',
-    //   title: "New Shoes 1",
-    //   amount: 69.99,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: "Groceries 2",
-    //   amount: 16.53,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: 't1',
+      title: "New Shoes 1",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: "Groceries 2",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: "New Shoes 3",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: "Groceries 4",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: "New Shoes 5",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: "Groceries 6",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: "New Shoes 7",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't8',
+      title: "Groceries 8",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't9',
+      title: "New Shoes 9",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't10',
+      title: "Groceries 10",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
 
   List<Transaction> get _recentTransactions {
@@ -89,7 +134,8 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
-  void _addNewTransaction(String txTitle, double txAmount, DateTime selectedDate) {
+  void _addNewTransaction(
+      String txTitle, double txAmount, DateTime selectedDate) {
     final newTx = Transaction(
         id: DateTime.now().toString(),
         title: txTitle,
@@ -109,8 +155,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    final appBarObj = AppBar(
         title: Text(
           "Personal Expenses",
         ),
@@ -120,21 +165,32 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () => _startAddNewTransaction(context),
           ),
         ],
-      ),
+      );
+    return Scaffold(
+      appBar: appBarObj,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              width: double.infinity,
+              // width: double.infinity,
+              height: (MediaQuery.of(context).size.height - 
+                        appBarObj.preferredSize.height - 
+                        MediaQuery.of(context).padding.top
+                        ) * 0.4,
               child: Card(
                 color: Colors.blue,
                 child: Chart(_recentTransactions),
                 elevation: 5,
               ),
             ),
-            TransactionList(_userTransations, _deleteTransaction), //step 2 - to pass pointer
+            Container(
+              height: (MediaQuery.of(context).size.height - appBarObj.preferredSize.height - 
+                        MediaQuery.of(context).padding.top) * 0.6,
+              child: TransactionList(_userTransations,
+                  _deleteTransaction),
+            ), //step 2 - to pass pointer
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,71 +8,70 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function deleteTx; //step3 - to pass pointer - add function
   //A constructor too
-  TransactionList(this.transactions, this.deleteTx); //step 4 - to pass pointer - change constructor
+  TransactionList(this.transactions,
+      this.deleteTx); //step 4 - to pass pointer - change constructor
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 600,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  "No transactions",
-                  style: Theme.of(context).textTheme.headline6,
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                "No transactions",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              //For empty space
+              SizedBox(
+                height: 10,
+              ),
+              //container for resizing and fitting the image
+              Container(
+                height: 200,
+                child: Image.asset(
+                  "assets/images/waiting.png",
+                  fit: BoxFit.cover,
                 ),
-                //For empty space
-                SizedBox(
-                  height: 10,
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
                 ),
-                //container for resizing and fitting the image
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    "assets/images/waiting.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            '\$${transactions[index].amount}',
-                          ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text(
+                          '\$${transactions[index].amount}',
                         ),
                       ),
                     ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      // style: Theme.of(context).textTheme.headline6,
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transactions[index].id), //step 5: to pass pointer - use it
-                    ),
                   ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    // style: Theme.of(context).textTheme.headline6,
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTx(transactions[index]
+                        .id), //step 5: to pass pointer - use it
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
