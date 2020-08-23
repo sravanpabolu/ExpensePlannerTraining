60f60b88915a0009beb404192b90251440b725a2
Sravan <sravanpabolu@gmail.com>
2020-08-23 13:13:46 +0530

    Fixing ShowChart text #131

--- lib/main.dart
+++ lib/main.dart
@@ -221,7 +221,8 @@ class _MyHomePageState extends State<MyHomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text("Show Chart"),
+                  Text("Show Chart",
+                  style: Theme.of(context).textTheme.headline6,),
                   Switch.adaptive(
                       activeColor: Theme.of(context)
                           .accentColor, //for not using default green color for switch
